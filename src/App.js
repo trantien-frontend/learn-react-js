77aa7180eb62fb58d1655f58db487a2ae81e89d6
TienTran <tientran0963598@gmail.com>
2021-03-15 00:26:08 +0700

    uppdate

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Tien Tran</p>
+        <p>Tien Tran a </p>
       </header>
     </div>
   );
